aad848e1a7fd9968a38f1332770f1b67aab196c2
StrawberryDr <1471612073@qq.com>
2020-02-15 14:18:57 +0800

    20200215

---
+++ 在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,63 @@
+public class solution {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        if(nums.length == 0){
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+        result[0] = findFirst(nums,nums.length,target);
+        result[1] = findLast(nums,nums.length,target);
+        return result;
+    }
+    public static int findFirst(int[] a,int len,int key){
+        if(len < 1){
+            return -1;
+        }
+        int low = 0;
+        int high = len - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(a[mid] == key && (mid - 1 < 0 || a[mid - 1] != key)){
+                return mid;
+            }else if(a[mid] >= key){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static int findLast(int [] a, int len, int key)
+    {
+        if (len < 1)
+            return - 1;
+        int low = 0;
+        int high = len - 1;
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            if (a[mid] == key && (mid + 1 >= len || a[mid + 1] != key))
+            {
+                return mid;
+            }
+            else if (a[mid] <= key)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int[] ret = searchRange(nums,8);
+        for(int x : ret){
+            System.out.println(x);
+        }
+    }
+}
